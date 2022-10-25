bdd16b5d3e6d755b6b1d3a5719ca4cb777382e95
palawatyoo <110587167+palawatyoo@users.noreply.github.com>
2022-10-25 15:50:18 +0700

    Add files via upload

--- verify.php
+++ verify.php
@@ -5,36 +5,29 @@
         die();
     }
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>verify</title>
-</head>
-<body>
-    <h1 align="center">Webboard TheToy</h1>
-    <hr>
-    <div align="center">
+
     <?php
+    $login=$_POST["login"];
+    $pwd=$_POST["pwd"];
     if($_POST['login'] == "admin" && $_POST['pwd'] == "ad1234" ){
-        echo"ยินดีต้อนรับคุณ ADMIN";
+
         $_SESSION['username']='admin';
         $_SESSION['role']='a';
         $_SESSION['id']=session_id();
+        header("location:index.php");
+        die();
 
     } elseif ($_POST['login'] == "member" && $_POST['pwd'] == "mem1234" ){
-        echo"ยินดีต้อนรับคุณ MEMBER";
+
         $_SESSION['username']='member';
         $_SESSION['role']='m';
         $_SESSION['id']=session_id();
+        header("location:index.php");
+        die();
 
     } else{
-        echo"ชื่อบัญชีหรือรหัสผ่านไม่ถูกต้อง";
+        $_SESSION['error']='error';
+        header("location:login.php");
+        die();
     }
 ?>
-    </div>
-    <center><a href="index.php">กลับไปหน้าหลัก</a></center>
-</body>
-</html>
